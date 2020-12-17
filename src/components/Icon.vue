aa892c0fbe7b82bddea5942eecb8679b73656c08
Pablo Miceli <d1urno@gmx.com>
2020-12-17 08:43:13 -0300

    workflow: setup icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <i
+    class="inline-flex justify-center items-center"
+    role="presentation"
+    :aria-labelledby="type"
+  >
+    <component :is="type"/>
+  </i>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  components: {},
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
