793cbba3a58198f3dbe181f8a55c47609dc80797
Pablo Miceli <d1urno@gmx.com>
2020-12-17 12:00:21 -0300

    feat: create initial button component

---
+++ src/components/VButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="py-3 font-bold uppercase bg-yellow-400 rounded-sm"
+          role="button"
+          :aria-labelledby="label">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'VButton',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
